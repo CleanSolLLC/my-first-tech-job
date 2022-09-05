b8277a9779c112a0ef60f9fc9f733bafbf126997
mamark90 <mamark90@gmail.com>
2022-09-04 20:47:39 -0400

    Refactored Devise log in screen using Tailwind template

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,4 +1,7 @@
 class Opportunity < ApplicationRecord
     has_many :user_opportunities
     has_many :users, through: :user_opportunities
+
+    validates :name, presence: true
+    validates :opportunity_type, presence: true
 end
